b1da7a330bd667a2262d5ac73d800cc3639b0ae1
SmileMan1235 <93837074+BlakeCrimminFan@users.noreply.github.com>
2021-11-09 17:04:47 +0000

    sus halal

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const blakesQuotes = [
     "I always loved Ellie. It's a shame I didn't stay with her, I still wank though!"
 ]
 
-const raulsQuotes = [
-    "Ellie and Bailey are my beep word idk I'm not that good",
-    "I have cheated on alot of woman",
-    "Who is ligma?",
-    "Fortnite",
-    "I love bailey but i dont know if she likes me"
-]
-
 function randomNumber(maxNumber) {
     return Math.floor(Math.random() * maxNumber)
 }
@@ -21,9 +13,4 @@ function randomNumber(maxNumber) {
 function blakesButtonClicked() {
     let text = `"${blakesQuotes[randomNumber(blakesQuotes.length)]}" -Blake`
     document.getElementById("BlakesFavouriteLine").textContent = text
-}
-
-function raulsButtonClicked() {
-    let text = `"${raulsQuotes[randomNumber(raulsQuotes.length)]}" -Raul`
-    document.getElementById("RaulsFavouriteLine").textContent = text
 }
